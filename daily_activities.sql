5231b6f32a67b79cbfba38e3299b9047bab0dc51
Sugarcone97 <37585767+Sugarcone97@users.noreply.github.com>
2021-01-25 21:43:43 +0100

    Add files via upload

---
+++ daily_activities.sql
@@ -0,0 +1,31 @@
+﻿CREATE DATABASE daily_activities
+CHARACTER SET utf8
+COLLATE utf8_hungarian_ci;
+
+USE daily_activities;
+
+CREATE TABLE activitytypes (
+  id int not NULL AUTO_INCREMENT PRIMARY KEY,
+  activityname varchar(40) NOT NULL UNIQUE,
+  activitytype varchar(40) NOT NULL);
+
+INSERT INTO activitytypes(activityname, activitytype)
+  VALUES ('Basketball', 'Sport'),
+         ('Football', 'Sport'),
+         ('Tennis', 'Sport'),
+         ('Piano', 'Music'),
+         ('Guitar', 'Music'),
+         ('Drums', 'Music'),
+         ('Drawing', 'Art'),
+         ('Painting', 'Art'),
+         ('Sculpting', 'Art');
+
+CREATE TABLE dailydata (
+  id int not NULL AUTO_INCREMENT PRIMARY KEY,
+  activityid int not NULL,
+  startdate datetime,
+  enddate datetime,
+  comment varchar(100) NOT NULL,
+  CONSTRAINT fk_dailydata_activityid FOREIGN key (activityid)
+    REFERENCES activitytypes (id)
+);
---
+++ readme.txt
@@ -0,0 +1,2 @@
+This Project uses Mysql database, please run daily_activities.sql on your local Mysql server. (server = localhost, user = root)
+Run the Exe file from here: Daily Activities Project/2019-01-07 Sportolok/bin/Release/DailyActivities_App.exe
